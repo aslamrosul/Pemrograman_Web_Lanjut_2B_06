acbfd07180ec796ea96969b288cd33d9bc8c60b3
aslamrosul <nvrsrndrmrx@gmail.com>
2025-03-05 00:01:24 +0700

    Praktikum2.4_Jobsheet4_Aslam RA_07

--- PWL_POS/app/Http/Controllers/UserController.php
+++ PWL_POS/app/Http/Controllers/UserController.php
@@ -10,13 +10,14 @@ class UserController extends Controller
 {
     public function index()
     {
-        
-       $user = UserModel::firstOrNew([
-            'username' => 'manager',
-            'nama' => 'Manager',
-        ],
-    );
+        $user = UserModel::firstOrNew([
+            'username' => 'manager33',
+            'nama' => 'Manager Tiga Tiga',
+            'password' => Hash::make('12345'),
+            'level_id' => 2
+        ]);
 
+        $user->save();
 
         return view('user', ['data' => $user]);
     }
