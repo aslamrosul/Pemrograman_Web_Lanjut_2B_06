df3ba87832d5e393b5f1f05aa1c3490b310bb505
aslamrosul <nvrsrndrmrx@gmail.com>
2025-03-04 23:48:29 +0700

    commit userblade

--- PWL_POS/resources/views/user.blade.php
+++ PWL_POS/resources/views/user.blade.php
@@ -1,19 +1,17 @@
-<!DOCTYPE html>
-<html lang="id">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Data User</title>
-</head>
 <body>
-    <h2>Data User</h2>
-    <table border="1">
+    <h1>Data User</h1>
+    <table border="1" cellpadding="2" cellspacing="0">
         <tr>
-            <th>Jumlah Pengguna</th>
+            <td>ID</td>
+            <td>Username</td>
+            <td>Nama</td>
+            <td>ID Level Pengguna</td>
         </tr>
         <tr>
-            <td>{{ $data }}</td>
+            <td>{{ $data->user_id }}</td>
+            <td>{{ $data->username }}</td>
+            <td>{{ $data->nama }}</td>
+            <td>{{ $data->level_id }}</td>
         </tr>
     </table>
 </body>
-</html>
