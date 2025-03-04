187b6b925dd54cabaa0ae5dbe46c4353f3a4aac1
aslamrosul <nvrsrndrmrx@gmail.com>
2025-03-04 19:31:35 +0700

    Praktikum4_Jobsheet3_Aslam RA_07

--- PWL_POS/database/seeders/KategoriSeeder.php
+++ PWL_POS/database/seeders/KategoriSeeder.php
@@ -11,7 +11,8 @@ class KategoriSeeder extends Seeder {
         $kategori = ['Elektronik', 'Pakaian', 'Makanan', 'Minuman', 'Aksesoris'];
         foreach ($kategori as $k) {
             DB::table('m_kategori')->insert([
-                'kategori_kode' => strtoupper(substr($k, 0, 3)) . rand(100, 999),
+               'kategori_kode' => strtoupper(substr($k,
+                 0, 3)) . rand(100, 999),
                 'kategori_nama' => $k,
                 'created_at' => now(),
                 'updated_at' => now()
