10f122be36ef213ff30f17e095520dc76c15fc02
aslamrosul <nvrsrndrmrx@gmail.com>
2025-03-04 23:31:40 +0700

    Praktikum2.3_Jobsheet4_Aslam RA_07

--- PWL_POS/resources/views/user.blade.php
+++ PWL_POS/resources/views/user.blade.php
@@ -1,23 +1,19 @@
 <!DOCTYPE html>
-<html>
+<html lang="id">
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Data User</title>
 </head>
 <body>
-    <h1>Data User</h1>
-    <table border="1" cellpadding="2" cellspacing="0">
+    <h2>Data User</h2>
+    <table border="1">
         <tr>
-            <th>ID</th>
-            <th>Username</th>
-            <th>Nama</th>
-            <th>ID Level Pengguna</th>
+            <th>Jumlah Pengguna</th>
         </tr>
-            <tr>
-                <td>{{ $data->user_id }}</td>
-                <td>{{ $data->username }}</td>
-                <td>{{ $data->nama }}</td>
-                <td>{{ $data->level_id }}</td>
-            </tr>
+        <tr>
+            <td>{{ $data }}</td>
+        </tr>
     </table>
 </body>
 </html>
