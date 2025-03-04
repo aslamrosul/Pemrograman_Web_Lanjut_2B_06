55a9ee205c277c59af06ba6c1a4b43a01d654de5
aslamrosul <nvrsrndrmrx@gmail.com>
2025-03-05 01:01:09 +0700

    Praktikum2.6_Jobsheet4_Aslam RA_07

--- PWL_POS/resources/views/user.blade.php
+++ PWL_POS/resources/views/user.blade.php
@@ -1,17 +1,25 @@
 <body>
     <h1>Data User</h1>
+    <a href="{{ url('/user/tambah') }}">+ Tambah User</a>
     <table border="1" cellpadding="2" cellspacing="0">
         <tr>
-            <td>ID</td>
-            <td>Username</td>
-            <td>Nama</td>
-            <td>ID Level Pengguna</td>
+            <th>ID</th>
+            <th>Username</th>
+            <th>Nama</th>
+            <th>Level</th>
+            <th>Aksi</th>
         </tr>
+        @foreach ($data as $row)
         <tr>
-            <td>{{ $data->user_id }}</td>
-            <td>{{ $data->username }}</td>
-            <td>{{ $data->nama }}</td>
-            <td>{{ $data->level_id }}</td>
+            <td>{{ $row->user_id }}</td>
+            <td>{{ $row->username }}</td>
+            <td>{{ $row->nama }}</td>
+            <td>{{ $row->level_id }}</td>
+            <td>
+                <a href="{{ url('/user/ubah/' . $row->user_id) }}">Ubah</a>
+                <a href="{{ url('/user/hapus/' . $row->user_id) }}">Hapus</a>
+            </td>
         </tr>
+        @endforeach
     </table>
 </body>
