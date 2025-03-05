acd81afe8f6aa2d72c2125a7b75ca98d3f0401c9
aslamrosul <nvrsrndrmrx@gmail.com>
2025-03-05 09:44:29 +0700

    Lupa-Commit_Praktikum2.3_Jobsheet4_Aslam RA_07

--- PWL_POS/resources/views/user.blade.php
+++ PWL_POS/resources/views/user.blade.php
@@ -1,30 +1,19 @@
-
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data User</title>
+</head>
 <body>
-    <h1>Data User</h1>
-    <a href="{{ url('/user/tambah') }}">+ Tambah User</a>
-    <table border="1" cellpadding="2" cellspacing="0">
+    <h2>Data User</h2>
+    <table border="1">
         <tr>
-            <td>ID</td>
-            <td>Username</td>
-            <td>Nama</td>
-            <td>ID Level Pengguna</td>
-            <td>Kode Level</td>
-            <td>Nama Level</td>
-            <td>Aksi</td>
+            <th>Jumlah Pengguna</th>
         </tr>
-        @foreach ($data as $d)
         <tr>
-            <td>{{ $d->user_id }}</td>
-            <td>{{ $d->username }}</td>
-            <td>{{ $d->nama }}</td>
-            <td>{{ $d->level_id }}</td>
-            <td>{{ $d->level->level_kode }}</td>
-            <td>{{ $d->level->level_nama }}</td>
-            <td>
-                <a href="{{ url('/user/ubah/' . $d->user_id) }}">Ubah</a>
-                <a href="{{ url('/user/hapus/' . $d->user_id) }}">Hapus</a>
-            </td>
+            <td>{{ $data }}</td>
         </tr>
-        @endforeach
     </table>
 </body>
+</html>
