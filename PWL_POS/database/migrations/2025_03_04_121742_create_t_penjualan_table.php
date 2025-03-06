d3f4e3c3a1622cb5b05ebe638eb71a000471d7e6
aslamrosul <nvrsrndrmrx@gmail.com>
2025-03-06 15:31:15 +0700

    commit typo

--- PWL_POS/database/migrations/2025_03_04_121742_create_t_penjualan_table.php
+++ PWL_POS/database/migrations/2025_03_04_121742_create_t_penjualan_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('t_penjualan', function (Blueprint $table) {
-            $table->id();
+            $table->id('penjualan_id');
             $table->timestamps();
         });
     }
