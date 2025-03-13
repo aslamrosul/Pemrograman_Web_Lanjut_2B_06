ec1ed89c6aa1d6e4e09768ce5a341abf440119cf
aslamrosul <nvrsrndrmrx@gmail.com>
2025-03-13 23:42:49 +0700

    Praktikum4_Jobsheet5_Filtering DB_07_Aslam

--- PWL_POS/app/Http/Controllers/UserController.php
+++ PWL_POS/app/Http/Controllers/UserController.php
@@ -27,15 +27,23 @@ class UserController extends Controller
 
         $activeMenu = 'user'; // set menu yang sedang aktif
 
-        return view('user.index', ['breadcrumb' => $breadcrumb, 'page' => $page, 'activeMenu' => $activeMenu]);
+       $level = LevelModel::all();
+
+        return view('user.index', ['breadcrumb' => $breadcrumb, 'page' => $page, 'level' => $level, 'activeMenu' => $activeMenu]);
     }
 
     // Ambil data user dalam bentuk json untuk datatables
 
     public function list(Request $request)
     {
+
+
         $users = UserModel::select('user_id', 'username', 'nama', 'level_id')
             ->with('level');
+
+        if ($request->level_id) {
+            $users->where('level_id', $request->level_id);
+        }
 
         return DataTables::of($users)
             // menambahkan kolom index / no urut (default nama kolom: DT_RowIndex)
@@ -152,21 +160,20 @@ class UserController extends Controller
     }
 
     public function destroy(string $id)
-{
-    $check = UserModel::find($id);
-    
-    if (!$check) {
-        // Mengecek apakah data user dengan ID yang dimaksud ada atau tidak
-        return redirect('/user')->with('error', 'Data user tidak ditemukan');
-    }
-    
-    try {
-        UserModel::destroy($id);
-        return redirect('/user')->with('success', 'Data user berhasil dihapus');
-    } catch (\Illuminate\Database\QueryException $e) {
-        // Jika terjadi error ketika menghapus data,  kembali ke halaman dengan pesan error
-        return redirect('/user')->with('error', 'Data user gagal dihapus karena masih terdapat tabel lain yang terkait dengan data ini');
-    }
-}
+    {
+        $check = UserModel::find($id);
 
+        if (!$check) {
+            // Mengecek apakah data user dengan ID yang dimaksud ada atau tidak
+            return redirect('/user')->with('error', 'Data user tidak ditemukan');
+        }
+
+        try {
+            UserModel::destroy($id);
+            return redirect('/user')->with('success', 'Data user berhasil dihapus');
+        } catch (\Illuminate\Database\QueryException $e) {
+            // Jika terjadi error ketika menghapus data,  kembali ke halaman dengan pesan error
+            return redirect('/user')->with('error', 'Data user gagal dihapus karena masih terdapat tabel lain yang terkait dengan data ini');
+        }
+    }
 }
