7be5aa1a9d9f445ee3fc77af30632f0d41faeadf
aslamrosul <nvrsrndrmrx@gmail.com>
2025-05-06 19:20:58 +0700

    Jobsheet 10_Tugas_Aslam_07

--- PWL_POS/routes/api.php
+++ PWL_POS/routes/api.php
@@ -3,6 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\KategoriController;
+use App\Http\Controllers\Api\BarangController;
+use App\Http\Controllers\Api\SupplierController;
+use App\Http\Controllers\Api\LevelController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -26,11 +31,37 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 
 //level tabel
-use App\Http\Controllers\Api\LevelController;
- 
  Route::get('levels', [LevelController::class, 'index']);
  Route::post('levels', [LevelController::class, 'store']);
  Route::get('levels/{level}', [LevelController::class, 'show']);
  Route::put('levels/{level}', [LevelController::class, 'update']);
  Route::delete('levels/{level}', [LevelController::class, 'destroy']);
+
+ //user tabel
+ Route::get('users', [UserController::class, 'index']);
+ Route::post('users', [UserController::class, 'store']);
+ Route::get('users/{user}', [UserController::class, 'show']);
+ Route::put('users/{user}', [UserController::class, 'update']);
+ Route::delete('users/{user}', [UserController::class, 'destroy']);
  
+ //katgeori tabel
+ Route::get('kategori', [KategoriController::class, 'index']);
+ Route::post('kategori', [KategoriController::class, 'store']);
+ Route::get('kategori/{kategori}', [KategoriController::class, 'show']);
+ Route::put('kategori/{kategori}', [KategoriController::class, 'update']);
+ Route::delete('kategori/{kategori}', [KategoriController::class, 'destroy']);
+ 
+ //barang tabel
+ Route::get('barang', [BarangController::class, 'index']);
+ Route::post('barang', [BarangController::class, 'store']);
+ Route::get('barang/{barang}', [BarangController::class, 'show']);
+ Route::put('barang/{barang}', [BarangController::class, 'update']);
+ Route::delete('barang/{barang}', [BarangController::class, 'destroy']);
+ 
+ 
+ //supplier tabel
+ Route::get('supplier', [SupplierController::class, 'index']);
+ Route::post('supplier', [SupplierController::class, 'store']);
+ Route::get('supplier/{supplier}', [SupplierController::class, 'show']);
+ Route::put('supplier/{supplier}', [SupplierController::class, 'update']);
+ Route::delete('supplier/{supplier}', [SupplierController::class, 'destroy']);
